bc3f2a57b7d529dd46967703bce7fe9f5d1e0b0e
Neelam Chanapati <neelam.chanapati@gmail.com>
2022-03-25 23:45:53 +0530

    Create_profile errors solved and data is saved in django database

--- activityautomation/faculty/admin.py
+++ activityautomation/faculty/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User_Profile, UP
 
 # Register your models here.
+admin.site.register(User_Profile)
+admin.site.register(UP)
--- activityautomation/faculty/models.py
+++ activityautomation/faculty/models.py
@@ -57,3 +57,13 @@ class User_Profile(models.Model):
     display_picture = models.FileField()
     def __str__(self):
         return self.fname
+
+#new model
+class UP(models.Model):
+    fn = models.CharField(max_length=200)
+    ln = models.CharField(max_length = 200)
+    tech = models.CharField(max_length=500)
+    mail = models.EmailField(default = None)
+    dp = models.FileField()
+    def __str__(self):
+        return self.fn
