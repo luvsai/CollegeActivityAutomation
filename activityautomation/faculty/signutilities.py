a6f04e2dc78acc387cdb3ee91c0a2d91a2817e59
Lavanya Sai Kumar Kantubhukta <65954982+luvsai@users.noreply.github.com>
2022-03-30 22:36:20 +0530

    sigin

--- activityautomation/faculty/signutilities.py
+++ activityautomation/faculty/signutilities.py
@@ -1,17 +1,7 @@
 import random
 import string
-from tkinter.messagebox import RETRY
-from .models import *
-from django.shortcuts import render
+
 def getsessionid():
     S = 20  # number of characters in the string.
     ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k=S))
     return str(ran)
-
-def isSessionIDValid(sessionid): # if valid  return F_Id
-    try :
-        record = activeFaculty.objects.get(sessionid = sessionid)
-        return record.F_Id
-    except:
-        return None
-
