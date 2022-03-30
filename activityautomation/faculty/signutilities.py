dcd3b8b825f4034c190491d3b9c6eb0330df9058
Lavanya Sai Kumar Kantubhukta <65954982+luvsai@users.noreply.github.com>
2022-03-30 22:53:45 +0530

    session

--- activityautomation/faculty/signutilities.py
+++ activityautomation/faculty/signutilities.py
@@ -5,3 +5,7 @@ def getsessionid():
     S = 20  # number of characters in the string.
     ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k=S))
     return str(ran)
+
+def isSessionIDValid(sessionid):
+    
+    pass
