2605965d453d6f570826ba0bdf2d5aed2afba34b
Lavanya Sai Kumar Kantubhukta <65954982+luvsai@users.noreply.github.com>
2022-04-03 21:32:27 +0530

    just

--- activityautomation/faculty/signin.py
+++ activityautomation/faculty/signin.py
@@ -1,4 +1,5 @@
 import imp
+import re
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import  *
@@ -7,6 +8,11 @@ from django.views.decorators.csrf import csrf_exempt
 import random
 import string
 from .signutilities import *
+
+
+
+
+
 
 
 
@@ -74,17 +80,64 @@ def Cjlogin(request):
             return HttpResponse("error")
     #elsee show the login page
 
+
+user=''
+pwd=''
+@csrf_exempt
+def crlogin(request):
+    global user,pwd
+    sessionid = request.session.get("sessionid","NOSessionID")
+    if sessionid == "NOSessionID" :#not logged in                        
+        if request.method == "POST": #do login and show the output
+            user = int(request.POST["F_RegId"])
+            pwd = request.POST["F_Password"] 
+            print("pwd" , pwd)
+            try :
+                record = faculty.objects.get(F_Id = user)
+            except:
+                return render(request,'error2.html')
+            if pwd == record.F_Password:
+                #generate session id 
+                temp = getsessionid()
+                #add session token to the database
+                request.session["sessionid"] = temp
+                sessionid = temp
+                AF = activeFaculty.objects.create(F_Id= user,sessionid= sessionid)
+                return render(request,'welcome.html')
+            else :
+                return render(request,'error2.html')
+        else:
+            return render(request,'login.html')
+    else: #verify session id
+        #is session id is valid
+        F_Id = isSessionIDValid(sessionid)
+        if  F_Id  != None:
+            record = faculty.objects.get(F_Id = F_Id)
+
+            return HttpResponse("Logged in as : "+str(record.F_Name))
+        else:
+            del request.session['sessionid']
+            return HttpResponse("Invalid session user logout")
+
+
+
+    #elsee show the login page
+
+
+
+
     
 @csrf_exempt
 def login(request):
     sessionid = request.session.get("sessionid","NOSessionID")
     if sessionid == "NOSessionID" :#not logged in
         temp = getsessionid()
+        #add session token to the database
         request.session["sessionid"] = temp
         sessionid = temp
         return HttpResponse("Not logged in ")
         
-       
+        
     return HttpResponse(sessionid +" <br>logged IN yayy!!!")
 
 @csrf_exempt
@@ -93,6 +146,10 @@ def logout(request):
     if sessionid == "NOSessionID" :#not logged in
         return HttpResponse("Not Logged in")
     else :
+        try :
+            activeFaculty.objects.get(sessionid=sessionid).delete()
+        except:
+            pass
         del request.session['sessionid']
         return HttpResponse("Succesfullly Logged out")
 
