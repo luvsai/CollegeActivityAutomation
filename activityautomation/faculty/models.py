42921ba85a6be34107d6345686cb64a79fb59b1e
Lavanya Sai Kumar Kantubhukta <65954982+luvsai@users.noreply.github.com>
2022-04-19 18:45:48 +0530

    d

--- activityautomation/faculty/models.py
+++ activityautomation/faculty/models.py
@@ -21,7 +21,7 @@ class publications(models.Model):
     P_Title = models.CharField(verbose_name="Publication Title", max_length=500)
     P_Joural_Name = models.CharField(default="", verbose_name="Journal Name", max_length=500)
     P_Indexing = models.CharField(default="", verbose_name="Indexing", max_length=45)
-    P_IF_State = models.CharField(default="", verbose_name="Impact Factor State", max_length=100)
+    P_IF_State = models.CharField(default="NO", verbose_name="Impact Factor State", max_length=100)
     P_IF_Value  = models.CharField(default="", verbose_name="If Yes what is the impact factor?", max_length=20)
     P_IF_Source = models.CharField(default="", verbose_name="Impact Facotr Source", max_length=20)
     P_DOP = models.DateTimeField(verbose_name="Date of Publications")#models.DateTimeField(default=False)
